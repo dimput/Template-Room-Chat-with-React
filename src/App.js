a1298dd892927d8adc1f13f9c12f1fbb329d291f
dimput <dimasputra@mail.ugm.ac.id>
2020-08-09 01:55:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="title">Room Chat</div>
-        <div className="wrapper">
-          <div className="room-chat">
-            <div className="message left">Hello</div>
-            <div className="message right">Hello</div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="title">Room Chat</div>
+          <div className="wrapper">
+            <div className="room-chat">
+              <div className="message left">Hello</div>
+              <div className="message right">Hello</div>
+            </div>
+            <input className="input-chat" placeholder="Type a message" autoFocus={true} />
           </div>
-          <input className="input-chat" placeholder="Type a message" autoFocus={true}/>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
